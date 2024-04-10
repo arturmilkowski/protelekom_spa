9c0c76c31ab7392d0be68043e69efc2bce67ac5d
Artur Miłkowski <artur-milkowski@tlen.pl>
2024-04-10 16:27:52 +0200

    Install vue

---
+++ vue/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="25" height="25" />
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+  <RouterView />
+</template>
+
