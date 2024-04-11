8ec5a8bcfdac16decc935a106876b748a0d9fe5e
Artur Miłkowski <artur-milkowski@tlen.pl>
2024-04-11 11:20:45 +0200

    Add the file service

--- laravel/app/Http/Controllers/Api/Admin/Product/ProductController.php
+++ laravel/app/Http/Controllers/Api/Admin/Product/ProductController.php
@@ -3,13 +3,14 @@
 namespace App\Http\Controllers\Api\Admin\Product;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Http\Response;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 use App\Http\Requests\Admin\Product\StoreProductRequest;
 use App\Http\Resources\Admin\Product\ProductResource;
 use App\Models\Admin\Product\Product;
+use App\Services\File;
 
 class ProductController extends Controller
 {
@@ -21,6 +22,15 @@ class ProductController extends Controller
     public function store(StoreProductRequest $request): JsonResponse
     {
         $validated = $request->validated();
+        
+        $file = $request->file('img');
+        if ($file) {
+            $extension = $file->extension();
+            $filename = $validated['slug'] . '.' . $extension;
+            File::store($request, config('settings.filepath.product'), $filename);
+            $validated['img'] = $filename;
+        }
+
         $product = Product::create($validated);
 
         return response()->json($product, 201);
@@ -34,6 +44,17 @@ class ProductController extends Controller
     public function update(StoreProductRequest $request, Product $product): JsonResponse
     {
         $validated = $request->validated();
+        
+        $file = $request->file('img');
+        if ($file) {
+            $extension = $file->extension();
+            $filename = $validated['slug'] . '.' . $extension;
+            $path = File::update($request, $product->img, config('settings.filepath.product'), $filename);
+            if ($path) {
+                $validated['img'] = $filename; // assign new path
+            }
+        }
+
         $product->update($validated);
 
         return response()->json($product, 200);
@@ -41,6 +62,9 @@ class ProductController extends Controller
 
     public function destroy(Product $product): Response
     {
+        if ($product->img) {
+            Storage::delete(config('settings.filepath.product') . $product->img);
+        }
         $product->delete();
 
         return response()->noContent();
---
+++ laravel/config/settings.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'filepath' => [
+        'blog' => 'public/images/blog/',
+        'product' => 'public/images/products/',
+        'type' => 'public/images/products/types/',
+    ],
+    'store_filepath' => [
+        'blog' => 'storage/images/blog',
+        'product' => 'storage/images/products',
+        'type' => 'storage/images/products/types',
+    ]
+];
