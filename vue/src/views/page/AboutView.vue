991c9848a39da5369a767c17356c7d4d1c39311d
Artur Miłkowski <artur-milkowski@tlen.pl>
2024-04-22 11:47:44 +0200

    Add the home page view

--- vue/src/views/page/AboutView.vue
+++ vue/src/views/page/AboutView.vue
@@ -1,3 +1,37 @@
+<script setup>
+import { ref } from 'vue'
+import { useStore } from '@/stores/store'
+import HeaderTwo from '@/components/HeaderTwo.vue'
+import AppAlert from '@/components/AppAlert.vue'
+
+const store = useStore()
+const collection = ref([])
+const error = ref(null)
+
+const { err, collection: res } = await store.all('api/about')
+error.value = err
+collection.value = res
+</script>
+
 <template>
-  <h1>This is an about page</h1>
+  <HeaderTwo>O aplikacji</HeaderTwo>
+  <AppAlert v-if="error" type="danger">{{ error.message }}</AppAlert>
+  <template v-else>
+    <div class="flex flex-row px-2 py-2 border-b-[1px] border-gray-300">
+      <span class="basis-48">Wersja PHP:</span>
+      <span>{{ collection.php }}</span>
+    </div>
+    <div class="flex flex-row px-2 py-2 border-b-[1px] border-gray-300">
+      <span class="basis-48">Wersja Laravel:</span>
+      <span>{{ collection.laravel }}</span>
+    </div>
+    <div class="flex flex-row px-2 py-2 border-b-[1px] border-gray-300">
+      <span class="basis-48">Środowisko:</span>
+      <span>{{ collection.environment }}</span>
+    </div>
+    <div class="flex flex-row px-2 py-2 border-b-[1px] border-gray-300">
+      <span class="basis-48">Użytkownik:</span>
+      <span>{{ collection.user.name }} &mdash; {{ collection.user.email }}</span>
+    </div>
+  </template>
 </template>
--- vue/src/views/page/HomeView.vue
+++ vue/src/views/page/HomeView.vue
@@ -1,5 +1,41 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+import { useStore } from '@/stores/store'
+import HeaderTwo from '@/components/HeaderTwo.vue'
+import AppAlert from '@/components/AppAlert.vue'
+const store = useStore()
+
+const products = ref([])
+const error = ref(null)
+const apiUrl = 'api/admins/products/products'
+
+const { err, collection } = await store.all(apiUrl)
+error.value = err
+products.value = collection.data
+</script>
 
 <template>
-  <h2>Index</h2>
+  <HeaderTwo>Index</HeaderTwo>
+  <AppAlert v-if="error" type="danger">{{ error.message }}</AppAlert>
+  <section
+    v-else
+    class="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-4"
+  >
+    <div v-for="(product, index) in products" :key="product.id" class="mb-12">
+      <div
+        class="border-y-[1px] hover:bg-gray-50 border-black p-4 h-full bg-contain bg-right bg-no-repeat hover:bg-cover hover:bg-center"
+        :style="{ backgroundImage: 'url(' + product.img + ')' }"
+      >
+        <div>{{ index + 1 }}</div>
+        <div>{{ product.brand }}</div>
+        <div>{{ product.category }}</div>
+        <div>{{ product.name }}</div>
+        <div>{{ product.description }}</div>
+        <div>img: {{ product.img }}</div>
+        <!-- <template v-if="product.img">
+          <img :src="product.img" />
+        </template> -->
+      </div>
+    </div>
+  </section>
 </template>
